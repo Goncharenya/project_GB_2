a2047bf2e8f2a2b3dd66b3ee922bab84e4254001
Tanya Goncharenya <kotmay@mail.ru>
2023-11-12 21:31:33 +0300

    html

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+let menu   = document.querySelector('.menu');
+let burger = document.querySelector(('.menu-top__line'));
+
+
+    menu.addEventListener("click", () => {
+        if (menu.contains('_active')){
+            console.log('yes!');
+    }})
